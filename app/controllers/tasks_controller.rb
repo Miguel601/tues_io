0434aac82cb71cebdad9bde7adfe8fb18f726dc3
Miguel601 <henriquez.miguel@outlook.com>
2019-05-09 12:19:52 -0400

    FULL CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,6 +14,27 @@ class TasksController < ApplicationController
         end
     end
 
+    def edit
+        @task = Task.find(params[:id])
+    end
+
+    def update
+        @task = Task.find(params[:id])
+        if @task.update(task_params)
+            redirect_to project_path(task_params[:project_id])
+        else
+            render :edit
+        end
+        
+    end
+
+    def destroy
+        @return_project = Task.find(params[:id])
+        Task.destroy(params[:id])
+        redirect_to project_path(@return_project.project_id)
+    end
+
+
     private
 
     def task_params
