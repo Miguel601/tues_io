1b10458b0343b3693ef426cfb91cf9130ef5af1b
Miguel601 <henriquez.miguel@outlook.com>
2019-05-09 10:22:02 -0400

    Almost done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,21 @@
 class TasksController < ApplicationController
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.create(task_params) 
+        if @task.valid?
+            redirect_to project_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:description, :complete, :user_id, :project_id)
+    end
 end
